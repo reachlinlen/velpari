a4bf9c79eb435becf4408a1a8fb808e7b3355d88
monbgul <monbgul@monbgul.cloud>
2024-10-19 15:05:56 +0530

    nav bar imp

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -23,20 +23,20 @@ export default function NavBar({pathname}: {pathname: string}) {
                 className="h-8 w-auto"
               /> */}
             </div>
-            <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+            <div className="flex gap-x-8 sm:ml-6 sm:flex sm:space-x-8">
               {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-              <a
-                href="/"
-                className={`inline-flex items-center border-b-2 ${pathname == "/" ? 'border-indigo-500' : 'border-transparent'} px-1 pt-1 text-sm font-medium text-gray-900`}
-              >
-                TDEF
-              </a>
-              <a
-                href="/plants"
-                className={`inline-flex items-center border-b-2 ${pathname == "/plants" ? 'border-indigo-500' : 'border-transparent'} px-1 pt-1 text-sm font-medium text-gray-900`}
-              >
-                Plants
-              </a>
+                <a
+                  href="/"
+                  className={`inline-flex items-center border-b-2 ${pathname == "/" ? 'border-indigo-500' : 'border-transparent'} px-1 pt-1 text-sm font-medium text-gray-900`}
+                >
+                  TDEF
+                </a>
+                <a
+                  href="/plants"
+                  className={`inline-flex items-center border-b-2 ${pathname == "/plants" ? 'border-indigo-500' : 'border-transparent'} px-1 pt-1 text-sm font-medium text-gray-900`}
+                >
+                  Plants
+                </a>
               {/* <a
                 href="#"
                 className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
